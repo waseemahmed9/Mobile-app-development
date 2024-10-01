dc8fd524bfd031cee69eb869975f41ecfe395e74
Waseem <wa1582012@gmail.com>
2024-09-30 20:28:42 -0700

    labs created

---
+++ lab 1/dartlabs/bin/dartlabs.dart
@@ -0,0 +1,5 @@
+import 'package:dartlabs/dartlabs.dart' as dartlabs;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dartlabs.calculate()}!');
+}
---
+++ lab 1/dartlabs/test/dartlabs_test.dart
@@ -0,0 +1,8 @@
+import 'package:dartlabs/dartlabs.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}

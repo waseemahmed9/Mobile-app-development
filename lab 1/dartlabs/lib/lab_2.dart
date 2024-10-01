fd2051db2f2bab392f5ebc8ab484e4f292537e54
Waseem <wa1582012@gmail.com>
2024-09-30 20:33:49 -0700

    lab 2

---
+++ lab 1/dartlabs/lib/lab_2.dart
@@ -0,0 +1,159 @@
+// Question 1
+
+// void main(){
+//   int a = 25;
+//   int b = 5;
+//   double result = a / b;
+//   print(result);
+// }
+
+// Question 2
+
+// void main(){
+//   int a = 25;
+//   int b = 5;
+//   double result = a / b;
+//   print(result);
+// }
+
+// Question 3
+
+// void main(){
+//   var a = 20;
+//   var b = 30;
+//   var c = (a<=b);
+//   print(c);
+// }
+
+// Question 4
+
+// void main(){
+//   var num1 = 30;
+//   var num2 = 30;
+//   var result = (num1 == num2);
+//   print(result);
+
+//  num1 = 20;
+//  num2 = 30;
+//  result = (num1 == num2);
+//  print(result);
+// }
+
+// Question 5
+
+// void main(){
+//   var num1 = 30;
+//   var num2 = 30;
+//   var result = (num1 != num2);
+//   print(result);
+
+//  num1 = 20;
+//  num2 = 30;
+//  result = (num1 != num2);
+//  print(result);
+// }
+
+// Question 6 
+
+// void main(){
+//   var a;
+//   a ??= 20;
+//   print(a);
+// }
+
+// Question 7
+
+// void main(){
+//  var a = 16;
+//  var b =  (a>=18)? "User is eligible to Vote": "User is not eligible to";
+//  print(b);
+// }
+
+// Question 8 
+
+// void main() {
+//   var totalAmount = 150; 
+//   var isPremiumMember = true; 
+
+//   var eligibilityMessage = (isPremiumMember || totalAmount > 100) 
+//       ? "User is eligible for a discount!" 
+//       : "User is not eligible for a discount.";
+
+//   print(eligibilityMessage);
+// }
+
+// Question 9
+
+// void main() {
+//   var isSubscribed = false; 
+//   var offerMessage = (!isSubscribed) 
+//       ? "Congratulations! You have received a promotional offer!" 
+//       : "Thank you for subscribing! No promotional offer for you.";
+
+//   print(offerMessage);
+// }
+
+// Question 10
+
+// void main() {
+//   var number = -5;
+
+//   var result = (number > 0) 
+//       ? "The number is positive." 
+//       : "The number is negative.";
+
+//   print(result);
+// }
+
+// Question 11
+
+// void main() {
+//   var number = 7; 
+//   var result = (number % 2 == 0) 
+//       ? "The number is even." 
+//       : "The number is odd.";
+
+//   print(result);
+// }
+
+// Question 12
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

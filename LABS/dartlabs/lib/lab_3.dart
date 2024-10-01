a35bb1cbe922ca59f4ce6d8c0fe6936787936a84
Waseem <wa1582012@gmail.com>
2024-09-30 22:47:44 -0700

    lab 3 added

---
+++ LABS/dartlabs/lib/lab_3.dart
@@ -0,0 +1,107 @@
+// List<dynamic> myList = [];
+
+// void main() {
+//   List<String> myList = ['apple', 'banana', 'cherry'];
+//   print(myList[0]);  
+//   print(myList[1]); 
+//   print(myList[2]);  
+// }
+
+
+// void main() {
+//   List<int> numbers = [1, 2, 3];
+//   numbers.add(4);
+//   print(numbers);
+// }
+
+// void main() {
+//   List<String> fruits = ['apple', 'banana', 'cherry'];
+//   fruits.removeAt(1);
+//   print(fruits); 
+// }
+
+// void main() {
+//   List<String> fruits = ['apple', 'banana', 'cherry'];
+//   int length = fruits.length;
+//   print('The number of fruits is: $length');
+// }
+
+// Map<String, int> myMap = {};
+
+// void main() {
+//   Map<String, int> ages = {
+//     'Alice': 30,
+//     'Bob': 25,
+//     'Charlie': 35,
+//   };
+//   print(ages);
+// }
+
+// void main() {
+//   Map<String, int> ages = {
+//     'Alice': 30,
+//     'Bob': 25,
+//     'Charlie': 35,
+//   };
+//   int aliceAge = ages['Alice']!; 
+//   print('Alice\'s age is: $aliceAge');
+// }
+
+// void main() {
+//   Map<String, int> ages = {
+//     'Alice': 30,
+//     'Bob': 25,
+//   };
+//   ages['Charlie'] = 35;
+//   ages['Diana'] = 28;
+//   ages['Alice'] = 32;
+//   print(ages); 
+// }
+
+// void main() {
+//   Map<String, int> ages = {
+//     'Alice': 30,
+//     'Bob': 25,
+//   };
+
+//   print('Number of entries: ${ages.length}');  // Output: Number of entries: 2
+// }
+
+
+
+// void main() {
+//   var a = 15;
+//   var b = 7;
+//   var sum = a + b;
+//   print(sum);
+// }
+
+// void main() {
+//   var num1 = 10.5;
+//   var num2 = 3.2;
+//   var product = num1 * num2;
+//   print(product);
+// }
+
+// void main() {
+//   var num = -8;
+//   print(num.abs());
+// }
+
+// void main() {
+//   var decimalNum = 7.3;
+//   print(decimalNum.ceil());
+//   print(decimalNum.floor());
+// }
+
+// void main() {
+//   var a = 10;
+//   var b = 5;
+//   if (a > b) {
+//     print('a is greater than b');
+//   } else {
+//     print('b is greater than a');
+//   }
+// }
+
+
